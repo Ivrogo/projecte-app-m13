68dc006dd80d5753f0395e608ec8a5d136e5e7f2
ivrogo <er_jotx@hotmail.com>
2023-03-08 15:56:31 +0100

    arreglado un problema con unas clases

--- src/main/java/com/movies4rent/Servidor/Controller/UsuariController.java
+++ src/main/java/com/movies4rent/Servidor/Controller/UsuariController.java
@@ -5,11 +5,6 @@ package com.movies4rent.Servidor.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author Er_jo
